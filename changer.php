a2b82c0d95c79dd88acc6079b6727505e9d4d201
impishMD <33032160+impishMD@users.noreply.github.com>
2019-01-30 10:47:36 +0300

    Add files via upload

---
+++ changer.php
@@ -0,0 +1,15 @@
+﻿<?php
+function randomPassword() {
+    $alphabet = "ABCDEFGHKLMNOPRSTXYZ123456789"; //
+    $len = 8; //
+    $pass = array();
+    $alphaLength = strlen($alphabet) - 1;
+    for ($i = 0; $i < $len; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass);
+}
+
+echo randomPassword();
+?>
